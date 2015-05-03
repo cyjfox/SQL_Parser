b67f8a77c77ecfb86bd7e25fb8f4b703defa144e
cyjfox <cyjfox@hotmail.com>
2015-05-03 13:05:04 +0800

    初步完成GetAllSubStrings的涉及
    Signed-off-by: cyjfox <cyjfox@hotmail.com>

--- SQL Parser/GetToken_test.cpp
+++ SQL Parser/GetToken_test.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <list>
+#include <vector>
 
 using namespace std;
+
+const int STRCMP_SAME = 0;
+const int STRCMP_NOTSAME = -1;
+
+bool GetAllSubString(char *str, const char * const substr, vector<unsigned int> *presult);
+bool GetSubStringFromLeft(char *str, const char * const substr, unsigned int *poffset);
+int cyj_strcmp(char *str, const char * const substr, unsigned int len);
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +21,10 @@ int main(int argc, char *argv[])
 	Buf = new char[1024];
 	strcpy(Buf, "unread");
 	cout<<Buf<<endl;
+
+	vector<unsigned int> *pSubStrings;
+	pSubStrings = new vector<unsigned int>();
+
 	if (file.fail())
 	//if (true)
 	{
@@ -28,15 +40,31 @@ int main(int argc, char *argv[])
 		file.seekg(0);
 		cout<<"get pointer is "<<file.tellg()<<endl;
 		file.getline(Buf, 1024, '\0');
-		//file>>Buf;
 		file.close();
+
+		
+
+		GetAllSubString(Buf, "test", pSubStrings);
+
+		vector<unsigned int>::iterator i, iend;
+		iend = pSubStrings->end();
+		unsigned int index;
+		index = 0;
+		for (i = pSubStrings->begin(); i < iend; i++)
+		{
+			cout<<"第"<<index<<"个字串的偏移为:"<<(*i)<<endl;
+			index++;
+		}
+		
 	}
 	
+
 	cout<<"Buf is "<<Buf<<endl;
+	delete pSubStrings;
 	return 0;
 }
 
-Vector<DWORD> *GetAllSubString(char *str, const char * const substr, char terminchar)
+bool GetAllSubString(char *str, const char * const substr, vector<unsigned int> *presult)
 {
 	if ((str == NULL) || (substr == NULL))
 	{
@@ -44,30 +72,62 @@ Vector<DWORD> *GetAllSubString(char *str, const char * const substr, char termin
 	}
 
 	/*
-	DWORD Count;
+	unsigned int Count;
 	Count = 0;
-	List<DWORD> tmpList;
-	tmpList = new List<DWORD>();
+	List<unsigned int> tmpList;
+	tmpList = new List<unsigned int>();
 	*/
 
-	Vector<DWORD> ResultVector;
-	ResultVector = new Vector<DWORD>
+	
 
-	DWORD offset;
+	unsigned int offset, totaloffset;
 	char *s1;
 	s1 = str;
+	totaloffset = 0;
 	//不断从左边获取子字符串
-	while (GetSubStringFromLeft(s1, substr, terminchar， &offset))
+	while (GetSubStringFromLeft(s1, substr, &offset))
 	{
 		s1 += offset;	//跳到已找到的子字符串
+		totaloffset += offset;	//保存找到的子字符串偏移
+		presult->push_back(totaloffset);
 		s1++;	//从下一个字符开始搜索
-
-		/*
-		tmpList.push_back(offset);	//保存找到的子字符串偏移
-		Count++;	//更新统计计数
-		*/
-		ResultVector.push_back(offset);
+		totaloffset++;		
 	}
 
-	return &ResultVector;
+	return true;
+}
+
+bool GetSubStringFromLeft(char *str, const char * const substr, unsigned int *poffset)
+{
+	char *s1;
+	s1 = str;
+
+	unsigned int len1;
+	len1 = strlen(substr);
+	unsigned int offset;
+	offset = 0;
+	while (*s1)	//如果没遇到结束字符0x0，则扫描
+	{
+		if (cyj_strcmp(s1, substr, len1) == STRCMP_SAME)	//当前位置字符串是否符合
+		{
+			*poffset = offset;
+			return true;
+		}
+		
+		offset++;
+		s1++;
+	}
+}
+
+int cyj_strcmp(char *str, const char * const substr, unsigned int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		if ((str[i] == '0x0') || (str[i] != substr[i]))
+		{
+			return STRCMP_NOTSAME;
+		}
+	}
+
+	return STRCMP_SAME;
 }
