1cfee24ec25e70fcf7d945413b9d99bc69fdbf57
cyjfox@hotmail.com <cyjfox@hotmail.com>
2015-07-16 15:17:21 +0800

    Signed-off-by: cyjfox@hotmail.com <cyjfox@hotmail.com>

--- SQL Parser/GetToken_test.cpp
+++ SQL Parser/GetToken_test.cpp
@@ -8,6 +8,27 @@ using namespace std;
 const int STRCMP_SAME = 0;
 const int STRCMP_NOTSAME = -1;
 
+
+class CTreeNode
+{
+public:
+	unsigned int loffset;//表达式开始的偏移
+	unsigned int roffset;//表达式结束的偏移
+	vector<CTreeNode *> *children;//子节点
+
+	CTreeNode()
+	{
+		loffset = 0;
+		roffset = 0;
+		children = new vector<CTreeNode *>();
+	}
+};
+
+//遍历树
+void tree_traverse(CTreeNode *proot);
+
+//解析成功返回true,出错失败返回false
+bool Parser(const char * const sqlstr);
 //case_sense表示是否对大小写敏感，true表示敏感，false表示不敏感
 bool GetAllSubString(const char * const str, const char * const substr, vector<unsigned int> *presult, bool case_sense);
 bool GetSubStringFromLeft(const char * const str, const char * const substr, unsigned int *poffset, bool case_sense);
@@ -23,6 +44,15 @@ int cyj_strcmp(const char * const str, const char * const substr, unsigned int l
 
 //上述至少应该改为每个FROM必然有一个select配对，因为有一种语法是select 常量，这时没有FROM配对
 
+
+// 指定在 SQL Server 2014 的 DELETE、SELECT 和 UPDATE 语句中使用的表、视图、派生表和联接表。 在 SELECT 语句中，FROM 子句是必需的，除非选择列表只包含常量、变量和算术表达式（没有列名）。 
+
+//继续假设，每个from和它对应的select之间是列名，先提取这一部分
+
+//from后面一定是数据源。可以判定后面表达式的值类型是不是数据源。比如两个表连接的表达式返回值的类型仍然是数据源
+//首先检索出 from后面的第一个合法元素，包括（即左括号，（左括号只有一个字符可以和from之间没有空格或者有空格，，表名这几种情况
+
+//SQL语句括号具有最高的运算级别，首先扫描括号的配对，执行最里层的括号内的表达式
 int main(int argc, char *argv[])
 {
 	cout<<"Hello, World!"<<endl;
@@ -81,12 +111,189 @@ int main(int argc, char *argv[])
 		
 	}
 	
+	Parser(Buf);
 
 	cout<<"Buf is "<<Buf<<endl;
 	delete pSubStrings;
 	return 0;
 }
 
+void tree_traverse(CTreeNode *proot)
+{
+	static int i = 0;
+	cout<<"i is:"<<i<<endl;
+	cout<<"loffset:"<<proot->loffset<<endl;
+	cout<<"roffset:"<<proot->roffset<<endl;
+	int n = proot->children->size();
+	if (n > 0)
+	{
+		i++;
+		for (int j = 0; j < n; j++)
+		{
+			tree_traverse(proot->children->at(j));
+		}
+		i--;
+
+	}
+
+}
+
+/*
+检查括号匹配情况，匹配正确返回true，同时构建一棵树返回,检查该树的子节点情况，确定有没有括号及括号的情况，匹配失败返回false
+*/
+//一个右括号总是跟在左边与它最近的左括号配对
+
+bool MatchBrackets(const char * const sqlstr, CTreeNode *ptree_exp)
+{
+	const char *s1;
+	s1 = sqlstr;
+
+	unsigned int offset = 0;
+
+	bool lfound = false;
+	CTreeNode *child;
+
+	unsigned int loffset = 0, roffset = 0;
+	cout<<"begin scanning"<<endl;
+	while (*s1)
+	{
+		if ((*s1) == '(' && lfound == false)//找到未匹配的左括号
+		{			
+			child = new CTreeNode();
+			loffset = offset + 1;//表达式开始
+			lfound = true;
+			cout<<"got a left bracket"<<" *s1:"<<*s1<<endl;
+			offset++;
+			s1++;
+			continue;
+		}
+
+		if ((*s1) == '(' && lfound == true)//找到里层的左括号
+		{
+			if (MatchBrackets(s1, child))//如果子串配对成功
+			{
+				//子串不可能匹配成功，因为父串的左括号还没匹配,如果是一个合法的表达式，肯定有一个右括号还没有匹配。而函数成功的情况只有扫描到末尾，并且之前的匹配全部成功，这种情况只可能发生在最外层的函数中。
+				cout<<"sub MatchBrackets returned true which means a error"<<endl;
+				return false;
+			}
+			else//配对失败,对这里的调用来说是正常的
+			{
+				//应该至少匹配了一个子串
+				if (child->children->size() < 1)
+				{
+					cout<<"sub Matchbrackets not matched!"<<endl;
+					return false;
+				}
+				else
+				{
+					cout<<"prepare to skip part that had been scanned!"<<endl;
+					//取最后一个表达式的右侧偏移，跳过匹配好的子串部分
+					unsigned int len1;
+					len1 = child->children->at(child->children->size() - 1)->roffset + 1;
+					s1 += len1;
+					offset += len1;
+					cout<<"skipped!len1 is "<<len1<<endl;
+				}
+			}
+			
+		}
+
+		if ((*s1) == ')' && lfound == false)
+		{
+			cout<<"found a right bracket without a left bracket"<<endl;
+			return false;
+		}
+
+		if ((*s1) == ')' && lfound == true)
+		{
+			roffset = offset;
+			lfound = false;//一组配对完毕，重置相关标志
+			child->loffset = loffset;
+			child->roffset = roffset;
+			ptree_exp->children->push_back(child);//一个完整的表达式获取完毕，可以添加到父节点
+			offset++;
+			s1++;
+			continue;
+		}
+
+		offset++;
+		s1++;
+	}
+
+
+	//如果扫描到了末尾
+	if (lfound == true)//如果仍然有未配对的左括号，则是一个不合法的表达式
+	{
+		//遍历树，清除已插入的节点，交给调用函数执行吧
+		return false;
+	}
+
+	//整个表达式是根节点
+	ptree_exp->loffset = 0;
+	ptree_exp->roffset = offset;
+	cout<<"offset is :"<<offset<<endl;
+	cout<<"sqlstr is :"<<sqlstr<<endl;
+	cout<<"s1 is :"<<s1<<endl;
+	return true;
+}
+
+/*
+bool MatchBrackets(const char * const sqlstr, CTreeNode *ptree_exp)
+{
+	vector<unsigned int> loffsets;
+	
+	const char *s1;
+	s1 = sqlstr;
+	unsigned int offset;
+	offset = 0;
+	while (*s1)
+	{
+		if (*s1 == '(')
+		{
+			loffsets.push_back(offset);
+		}
+		else if (*s1 == ')')
+		{
+			ptree_exp->
+		}
+
+}
+*/
+
+bool Parser(const char * const sqlstr)
+{
+	//先配对括号
+	CTreeNode *tree_exp;
+	tree_exp = new CTreeNode();
+	if (MatchBrackets(sqlstr, tree_exp))//true表示括号匹配正确，同时返回表达式树，false表示匹配失败
+	{
+		//遍历树输出
+		CTreeNode *currnode;
+		currnode = tree_exp;
+		
+		{
+			/*
+			cout<<"loffset "<<currnode->loffset<<endl;
+			cout<<"roffset "<<currnode->roffset<<endl;
+
+			currnode = currnode->children->at(0);
+			cout<<"loffset "<<currnode->loffset<<endl;
+			cout<<"roffset "<<currnode->roffset<<endl;
+			*/
+			tree_traverse(currnode);
+		}
+	}
+	else
+	{
+		cout<<"括号匹配失败！非法的表达式"<<endl;
+		return false;
+	}
+	//先判断句型（type)，表明整个表达式（即最外层）的句型为select,update,insert等
+	//如果是select句型
+	//先查找" from"子字串，即前面一个空格连接from（大小写不敏感）形成的字串
+
+	return true;
+}
 /*
 得到所有子串的位置,将每个子串的偏移按顺序填充到一个vector容器中
 */
